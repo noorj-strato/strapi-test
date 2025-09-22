35b9579b2072dc4d98427b85d46f40018502e341
noorj-strato <noor.joomratty@strato.de>
2025-09-22 09:19:28 +0000

    Implement custom find action in page controller to include current date in response metadata

--- my-strapi-project/src/api/page/controllers/page.ts
+++ my-strapi-project/src/api/page/controllers/page.ts
@@ -7,5 +7,14 @@ import { factories } from '@strapi/strapi'
 export default factories.createCoreController('api::page.page', ({ strapi }) => ({
     async customAction(context) {
         context.body = 'ok';
+    },
+    async find(context) {
+        // Call the default core action
+        const { data, meta } = await super.find(context);
+
+        const currentDate = new Date();
+        meta.date = currentDate.toISOString();
+        // Customize the response
+        return { data, meta };
     }
 }));
